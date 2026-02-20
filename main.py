210008f6a54b9a2c3c70f7fc524c10a464514564
potexxi <maximilian.schreiter@outlook.at>
2026-02-20 09:41:03 +0100

    Finish bare-bone

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-import discord
-from discord.ext import commands
-from requests import post
 
+from requests import post
+import public_ip
+
+username = "IP-Bot"
+webURL = "https://discordapp.com/api/webhooks/1474316366969569438/Warc3c07OSlvEahKQgTNfWJlBloYkIa1XiJalhN7LyouSJEOrAdJFDhyjiy02XivY93T"
+
+def sendmessage(content: str):
+    data ={
+        "content": content,
+        "username": username
+    }
+    post(webURL, data=data)
 
 def main():
-
+    ip = public_ip.get()
+    sendmessage(ip)
 
 if __name__ == "__main__":
     main()
