ef7777583ca465926a2561ed1f258cbff0af1b49
potexxi <maximilian.schreiter@outlook.at>
2026-02-20 11:32:02 +0100

    Update main.py and config file

--- main.py
+++ main.py
@@ -2,28 +2,43 @@ import json
 from requests import post
 import public_ip
 
-username = ""
-webURL = ""
-
-def sendmessage(content: str):
+def sendmessage(content: str, name: str, url: str):
     """
     Send the message to the weburl.
     :param content: the message-content.
+    :param name: the name, the bod should show in the discord message
+    :param url: the url, of the discord webhook
     """
     data = {
         "content": content,
-        "username": username
+        "username": name
     }
-    post(webURL, data=data)
+    post(url, data=data)
+
+def decode(message: str):
+    """
+    decode the message in the config.json file.
+    :param message: the message to decode
+    :return: first and second half of the message
+    """
+    list = message.split("<")
+    firsthalf = list[0]
+    secondlist = list[1].split(">")
+    secondhalf = secondlist[1]
+    return firsthalf, secondhalf
 
 def main():
     with open("config.json", "r") as f:
         config = json.load(f)
-    webURL = config["webURL"]
+    weburl = config["webURL"]
     username = config["bot_username"]
     ip = public_ip.get()
-    content = f"Server-IP: {ip} \n\n@everyone"
-    sendmessage(content)
+    first, second = decode(config["message"])
+    content = f"{first} {ip} {second}"
+    try:
+        sendmessage(content, username, weburl)
+    except:
+        print("[ERROR]: url or message is in wrong format. Please look in your config file!")
 
 if __name__ == "__main__":
     main()
