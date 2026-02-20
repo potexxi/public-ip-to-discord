2a5ca4b37419ca03286a57c56236ff3653bc1c26
potexxi <maximilian.schreiter@outlook.at>
2026-02-20 15:40:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,14 @@ sudo mkdir -p /opt/pubipdiscord
 sudo curl -o /opt/pubipdiscord/main.py https://raw.githubusercontent.com/potexxi/public-ip-to-discord/refs/heads/main/main.py
 sudo curl -o /opt/pubipdiscord/config.json https://raw.githubusercontent.com/potexxi/public-ip-to-discord/refs/heads/main/config.json
 sudo chmod +x /opt/pubipdiscord/main.py
-echo "Installation complete."
-echo "Start with: python3 /opt/pubipdiscord/main.py"
-echo "Please go to /opt/pubipdiscord and change the config.json to your discord webhook-url!!!"
 sudo curl -o /etc/systemd/system/pubipdiscord.service https://raw.githubusercontent.com/potexxi/public-ip-to-discord/refs/heads/main/pubipdiscord.service
 sudo systemctl daemon-reload
+sudo systemctl enable pubipdiscord
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "Installation complete."
+echo "sudo systemctl start pubipdiscord"
+echo "Please go to /opt/pubipdiscord and change the config.json to your discord webhook-url!!!"
