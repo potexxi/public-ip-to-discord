e758a968018804eaacf414a941e3124a1a953d9e
potexxi <maximilian.schreiter@outlook.at>
2026-02-20 14:51:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo mkdir -p /opt/pubipdiscord
 sudo curl -o /opt/pubipdiscord/main.py https://raw.githubusercontent.com/potexxi/public-ip-to-discord/refs/heads/main/main.py
-sudo curl -o /opt/pubipdiscord/config.json https://raw.githubusercontent.com/potexxi/public-ip-to-discord/refs/heads/main/config.json?token=GHSAT0AAAAAADTVIGLSV2LKE53ENBW6UFXU2MYMA6A
+sudo curl -o /opt/pubipdiscord/config.json https://raw.githubusercontent.com/potexxi/public-ip-to-discord/refs/heads/main/config.json
 sudo chmod +x /opt/pubipdiscord/main.py
 echo "Installation complete."
 echo "Start with: python3 /opt/pubipdiscord/main.py"
 echo "Please go to /opt/pubipdiscord and change the config.json to your discord webhook-url!!!"
-sudo curl -o /etc/systemd/system/pubipdiscord.service https://raw.githubusercontent.com/potexxi/public-ip-to-discord/refs/heads/main/pubipdiscord.service?token=GHSAT0AAAAAADTVIGLSBZXYTDGOATEEKFJW2MYMSMA
+sudo curl -o /etc/systemd/system/pubipdiscord.service https://raw.githubusercontent.com/potexxi/public-ip-to-discord/refs/heads/main/pubipdiscord.service
 sudo systemctl daemon-reload
